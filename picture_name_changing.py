76a9ac345a6fb1565853512f4df8795bcc895908
Muhammad Azeem <Azeemaj101@gmail.com>
2020-12-31 13:05:03 +0500

    Adding Comments

--- picture_name_changing.py
+++ picture_name_changing.py
@@ -1,13 +1,14 @@
+# Muhammad Azeem #  Number = 03244064060
 import os
 dic = []
 f = 1
-image_dir = r"F:\pic1"
+image_dir = r"F:\pic1"  # Add Your Folder Path
 all_files = os.listdir(image_dir)
 dic = all_files
 
 for i in all_files:
     path = os.path.join(image_dir,i)
-    a = f"{f}.jpg"
+    a = f"{f}.jpg"  # Add Extension
     p = os.path.join(image_dir, a)
     f = f+1
     os.rename(path, p)
